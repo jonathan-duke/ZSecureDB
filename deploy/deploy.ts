d9b54eca0c0256f3b48ee1da2250aaf84d6ce5b5
jonathan-duke <aacjuuwn463@outlook.com>
2025-11-27 14:27:40 +0800

    Update deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedDatabase = await deploy("ZSecureDatabase", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ZSecureDatabase contract: `, deployedDatabase.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_zsecure_database"; // id required to prevent reexecution
+func.tags = ["ZSecureDatabase"];
